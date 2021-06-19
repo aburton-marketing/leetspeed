4402999db8f5964e2e4f6548874eee26bf2a2646
Aaron Burton <81588424+aburton-marketing@users.noreply.github.com>
2021-06-19 15:53:35 -0700

    merged with formatter.py

--- routes.py
+++
@@ -1,4 +0,0 @@
-
-inp = open("C:\\Users\\user\\Documents\\GitHub\\leetcode_solution_compare_tool\\input.txt", 'r+')
-out = open("C:\\Users\\user\\Documents\\GitHub\\leetcode_solution_compare_tool\\output.txt", 'r+')
-# src = open("C:\\Users\\user\\Documents\\GitHub\\leetcode_solution_compare_tool\\src\\src_one.txt", 'r+')
