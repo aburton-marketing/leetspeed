0e90949e48d80e2222aa9aa53fecc7feef15db19
Aaron Burton <Aaronkeithburton@gmail.com>
2021-06-19 00:40:00 -0700

    routes for file sources
    Replace with your own directories

---
+++ routes.py
@@ -0,0 +1,4 @@
+
+inp = open("C:\\Users\\user\\Documents\\GitHub\\leetcode_solution_compare_tool\\input.txt", 'r+')
+out = open("C:\\Users\\user\\Documents\\GitHub\\leetcode_solution_compare_tool\\output.txt", 'r+')
+# src = open("C:\\Users\\user\\Documents\\GitHub\\leetcode_solution_compare_tool\\src\\src_one.txt", 'r+')
