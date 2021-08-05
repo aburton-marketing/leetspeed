46a136426010d2cc70844acb3449942f4208f4ae
aburton-marketing <81588424+aburton-marketing@users.noreply.github.com>
2021-08-05 09:29:56 -0700

    Compare tool. Pseudocode
    Non functional explanation of how we will be implementing our code.

---
+++ compare.py
@@ -0,0 +1,9 @@
+# Explination of program
+
+# for f in files: <- Pseudocode
+    # open file
+    # copy code
+    # run function on code
+    # get problem number & compile speed
+    # add values to database
+    # close file
