5fb902454e2d489b50beb225df447c5abce21725
aburton-marketing <81588424+aburton-marketing@users.noreply.github.com>
2021-08-05 11:33:21 -0700

    Pulling main functions out of the output.py file

--- compare.py
+++ compare.py
@@ -1,9 +1,42 @@
-# Explination of program
+import os
+import sys
+import csv
+import timeit
+import subprocess
+import output
+import formatter
+from shutil import copyfile
+from os import listdir
+from os.path import isfile, join
 
-# for f in files: <- Pseudocode
-    # open file
-    # copy code
-    # run function on code
-    # get problem number & compile speed
-    # add values to database
-    # close file
+
+# import timeit : done
+# itterate through files :
+# open file : done
+# import file : done
+# copy code :
+# run timeit function on code :
+# get problem number & compile speed :
+# add values to database :
+# close file : done
+
+main_func = None
+
+fs = open("C:\\Users\\abasp\\Documents\\github\\leetspeed\\output.py", 'r+')
+
+for line in fs:
+    if 'def' in line:
+        print(line)
+
+# def compare(a=1):
+#     for i in range(a):
+#         pass
+
+# print(timeit.timeit(compare, number=100000))
+
+fs.close()
+
+
+
+
+
